d9554f324ebe099cb497419be9229beef271bedb
Andreas Knöpfle <andi@bitcrowd.net>
2020-03-23 18:15:03 +0100

    Purge unused CSS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withCSS = require('@zeit/next-css');
+const withPurgeCSS = require('next-purgecss');
+/* eslint-enable @typescript-eslint/no-var-requires */
 
-module.exports = withCSS({});
+module.exports = withCSS(
+  withPurgeCSS({
+    purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer),
+  }),
+);
