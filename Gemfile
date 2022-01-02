2152f83712cbda7163a5e99f5992a0b908e3a447
Andreas Knöpfle <andi@bitcrowd.net>
2022-01-03 00:17:47 +0100

    Move to middleman (#33)

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'rubocop'
+gem 'tzinfo-data', platforms: %i[mswin mingw jruby x64_mingw]
+gem 'wdm', '~> 0.1', platforms: %i[mswin mingw x64_mingw]
---
+++ config.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = 'last 2 versions'
+end
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page '/google*.html', layout: false
+
+config[:title] = 'smallbutton.com - Andreas Knöpfle'
+config[:url] = 'https://smallbutton.com'
+config[:author] = '@andreasknoepfle'
+config[:description] = 'Yet another code monkeys website.'
+
+helpers do
+  def title
+    [config[:title], current_page.data.title].compact.join(' - ')
+  end
+
+  def description
+    current_page.data.description || config[:description]
+  end
+
+  def url(path)
+    "#{config[:url]}/#{path}"
+  end
+end
